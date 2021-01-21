851425c4fedcfedbb59a5104a6f2a5311476f868
jaaack233 <jackchen990227@gmail.com>
2021-01-21 19:55:51 +0800

    save

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,23 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text('Please wait ...');
+		$(".project").click(projectClick);
+		$(".jumbotron p").toggleClass("active");
 	});
-
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	console.log('Project Clicked');
+	e.preventDefault();
+	$(this).css("background-color", "green");
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		$(description).fadeOut();
+	}
 }
